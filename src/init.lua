ab1c2c789ec021fcafad033371498016cddde8b7
Ryan Jeffrey <ryan@ryanmj.xyz>
2020-09-19 22:57:09 -0700

    lua metatable rid global

--- src/init.lua
+++ src/init.lua
@@ -1,30 +1,9 @@
-local joestar_globals = {}
+local joestar = {}
 
- setmetatable(_G, {
+ setmetatable(joestar, {
     __newindex = function(t, n, v)
         jsync_l(n, v)
-        rawset(joestar_globals, n, v)
-    end,
-    __index = function(_, n)
-        if not joestar_globals[n] then
-            error("Attempt to read from undeclared variable: " .. n)
-        else
-            return joestar_globals[n]
-        end
+        rawset(joestar, n, v)
     end,
 })
-
-joestar = {}
-
-function get_variable(name)
-    return joestar_globals[name]
-end
- 
-function variable_sync(name, value)
-    rawset(joestar_globals, name, value)
-end
-
-joestar.get_variable = get_variable
-joestar.variable_sync = variable_sync
-
 return joestar
